2c110d1b402c0543dbe8a6f045dc8e00e819a56e
Cafu-DELA <guilherme.portela.reis@escola.pr.gov.br>
2024-10-06 20:54:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual é o jogador que tem mais gols pelo são paulo?',
+    'serginho chulapa'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual time ganhoy a champions league em 2019?',
+    'O Bayern de Munique'
+)
+
+criaCartao(
+    'Futebol',
+    'Quantos gols o calleri tem?',
+    'Ele tem 204 gols '
+)
+
+criaCartao(
+    'Futebol',
+    'Quando foi o ultimo titulo do são paulo?',
+    'Foi em 2021'
+)
